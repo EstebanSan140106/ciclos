92a2db3815f1130ac26c2fa3cf44bd9429fb2cda
Esteban <estebanasp8@gmail.com>
2024-10-17 14:40:59 -0500

    Clase 4.3 - Desarrollo Web 1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.write("Hola Mundo" + "<br>");
+document.write("Hola Mundo"+ "<br>");
+document.write("Hola Mundo" + "<br>"); 
+// For es un bucle  que tiene 3 parametros
+// la funciones es repetir un bloque las veces se sea necesario
+
+//sintaxis basica
+for(i=3;i<=15;i++) {
+    document.write("Hola desde js" + "<br>");
+    if (i%2==0) {
+        document.write(i+"<br>");
+    }
+}
